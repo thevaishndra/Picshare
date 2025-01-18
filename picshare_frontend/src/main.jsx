d3fff6bed479bf64793ffc3fed3a45f4383a62d9
thevaishndra <chandrav306m@gmail.com>
2025-01-18 13:13:41 +0530

    google login progress

--- picshare_frontend/src/main.jsx
+++ picshare_frontend/src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const clientId = "YOUR_GOOGLE_CLIENT_ID";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={clientId}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
