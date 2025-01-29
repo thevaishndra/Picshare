bcce97197962f2ceb76ca5bef88ff996d4d59f0e
thevaishndra <chandrav306m@gmail.com>
2025-01-30 02:09:42 +0530

    feed section

--- picshare_frontend/src/utils/data.js
+++ picshare_frontend/src/utils/data.js
@@ -2,3 +2,52 @@ export const userQuery = (userId) => {
     const query = `*[_type == "user" && _id == '${userId}']`;
     return query;
 }
+
+export const searchQuery = (search) => {
+    const query = `*[_type == "pin" && title match '${search}*' || category match '${search}*' || about match '${search}*']{
+      image {
+        asset -> {
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy -> {
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy -> {
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+}//adding asterisk means to search and match word even before completing it
+
+export const feedQuery = `*[_type == 'pin'] | order(_createAt desc){
+image {
+        asset -> {
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy -> {
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy -> {
+          _id,
+          userName,
+          image
+        },
+      },
+}` //newest post will be shown on top 
